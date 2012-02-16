6218de86237f1af614c70e8e20633b4430d02de6
Todd Wegter <twegter@iastate.edu>
2012-02-16 05:18:09 +0000

    Update to script.js and FieldAppTardy.html:
    I got sorting by chronological order enabled for printing using some crazy arrays. It works. Well. Beautifully? No. But well :) I also changed some basic operation so that the page never reloads. Functions simply update the necessary components. This allows the tardy list to stay up if new tardies are added. I added (good?) comments. Hopefully helpful. Feel free to use anything you need. I got rid of the nextID global variable and simply read localStorage every time I need it. I also found out how to pop up an alert (change to if(validate()) in line 28 to see the popup). I developed a method for clearing only desired entries from localStorage, and I figured out how to wait to reactivate buttons so the user can click other stuff while localStorage is being 'cleared'. As you can see, the validate() function also works amazingly. Not.

--- mobile app trunk/script.js
+++ mobile app trunk/script.js
@@ -1,12 +1,23 @@
-window.addEventListener('load',loaded,true);
-
 <!-- hide script from old browsers
 
-function loaded() {
-    var length = window.localStorage.length;
-    document.querySelector('#local-count').innerHTML = length;
-    document.querySelector('#submit').addEventListener('click',processData,false);
-    alert('loaded!');
+var absentPrepend = "absentStudent";
+var eventPrepend = "storedEvent";
+var tardyPrepend = "tardyStudent";
+
+function stringContains(sub, str) {            
+	return str.indexOf(sub) !== -1;
 }
+
+function dateToday() {
+	var d = new Date();        
+	return d.getFullYear()+"-"+(d.getMonth()+1)+"-"+d.getDate();
+}
+
+function timeNow() {
+	var d = new Date();
+	return d.getHours()+":"+d.getMinutes();
+} 
+
+
 
 //end hiding script from old browsers -->
