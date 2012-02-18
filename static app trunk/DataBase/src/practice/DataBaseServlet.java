ce00bc32920d949de1b3e81f3a38cf5d96140fbc
Brandon Maxwell <bmaxwell@iastate.edu>
2012-02-18 22:02:37 +0000

    New Version

--- static app trunk/DataBase/src/practice/DataBaseServlet.java
+++ static app trunk/DataBase/src/practice/DataBaseServlet.java
@@ -1,6 +1,9 @@
 package practice;
 
 import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
+
 import javax.servlet.http.*;
 
 import com.google.appengine.api.datastore.Entity;
@@ -19,19 +22,35 @@ public class DataBaseServlet extends HttpServlet
 ////		out.println("This is my nameSpace: " + found.getNamespace());
 //	}
 	
-	public void doGet(HttpServletRequest req, HttpServletResponse resp)throws IOException {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)throws IOException 
+	{
 		resp.setContentType("text/plain");
 		//PrintWriter out = resp.getWriter();
 		resp.getWriter().println("Allows you to add a person");
+		List<Person> students = makeStudents();
 		
-		Person toAdd = new Person("bmaxwell", "Brandon", "Maxwell", "password", "Computer Science", 
-				"Gloria", "Tenors");
-		DatastoreUtil.addPerson(toAdd);
+		DatastoreUtil.deleteEntity("bmaxwell");
+		
+		DatastoreUtil.addPerson(students.get(0));
 		
 		Entity found = DatastoreUtil.findEntity("bmaxwell");
 		if (found == null) 
 			resp.getWriter().println("Didn't find me");
 		else
-			resp.getWriter().println("This is my nameSpace: " + found.getNamespace());
+			resp.getWriter().println("Found Me!");
+	}
+	
+	private List<Person> makeStudents()
+	{
+		List<Person> list = new LinkedList<Person>();
+		list.add(new Person("bmaxwell", "Brandon", "Maxwell", "password", "Computer Science", 
+				"Gloria", "Tenors"));
+		list.add(new Person("cullerich", "Curtis", "Ullerich", "password", "Computer Engineer", 
+				"Vicky", "Uphonium"));
+		list.add(new Person("twegter", "Todd", "Wegter", "password", "Computer Engineer", 
+				"Vicky", "Sax"));
+		list.add(new Person("yhz", "Fei", "Zhu", "password", "Computer Science", 
+				"Gloria", "Piano"));
+		return list;
 	}
 }
