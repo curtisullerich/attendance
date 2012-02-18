0849953b80bd4eba297eaef85dd2c8399d8486de
Todd Wegter <twegter@iastate.edu>
2012-02-18 08:19:16 +0000

    FieldAppTardy.html now uses standard keys (pattern for which is shown in script.js) Tardy app also now checks tardies against events. Curtis and I now need to adapt the absence app to these standards

--- mobile app trunk/script.js
+++ mobile app trunk/script.js
@@ -21,19 +21,59 @@ function dateToday() {
 
 function timeNow() {
 	var d = new Date();
-	return d.getHours()+":"+d.getMinutes();
+	var hours = d.getHours();
+	var minutes = d.getMinutes();
+	if(parseInt(hours)<10)
+		hours = "0"+hours;
+	if(parseInt(minutes)<10)
+		minutes = "0"+minutes;
+	return hours+minutes;
 } 
 
-//will return name assoc. with netID from database
-function getName(netID){
-	return "firstname lastname";
+function twelveHourTimeNow(){
+	var d = new Date()
+	var hours = d.getHours();
+	var ampm;
+	if((parseInt(hours)) < 12)
+		ampm = "am";
+	else
+		ampm = "pm";
+	if(parseInt(hours) == 0)
+		hours = 12;
+	else
+		hours = parseInt(hours)%12;
+	var minutes = d.getMinutes();
+	if(parseInt(minutes)<10)
+		minutes = "0"+minutes;
+	return hours+":"+minutes+" "+ampm;
 }
 
-function localStorageContains(searchKey){
+//will return name assoc. with netID from database
+function getFirstName(netID){
+	return "firstname";
+}
+
+//will return name assoc. with netID from database
+function getLastName(netID){
+	return "lastname";
+}
+
+function localStorageContains(prepend, searchKey){
 	for(var i = 0, l = localStorage.length; i<l; i++)
 	{
 		var key = localStorage.key(i);
-		if(stringContains(searchKey,key))
+		if(stringContains(prepend,key) && stringContains(searchKey,key))
+			return key;
+	}
+	return null;
+}
+
+
+function localStorageContainsEvent(time){
+	for(var i = 0, l = localStorage.length; i<l; i++)
+	{
+		var key = localStorage.key(i);
+		if(stringContains(eventPrepend,key) && keyDelimiter(key, "starttime")<time && keyDelimiter(key, "endtime")>time)
 			return true;
 	}
 	return false;
@@ -42,8 +82,8 @@ function localStorageContains(searchKey){
 /**
 * Stores an entry with the standard key: prepend firstname lastname netID date startTime endTime
 **/
-function storeEntry(prepend, netID, date, startTime, endTime, entry){
-	var key = prepend+" "+getName(netID)+" "+netID+" "+date+" "+startTime+" "+endTime;
+function storeEntry(prepend, firstname, lastname, netID, date, startTime, endTime, entry){
+	var key = prepend+" "+firstname+" "+lastname+" "+netID+" "+date+" "+startTime+" "+endTime;
 	localStorage.setItem(key,entry);
 }
 
@@ -64,31 +104,34 @@ function clearLocalStoragewithPrepend(prepend){
 * This function delineates the key into an array (index 0=prepend, 1=firstname, 2=lastname, 3=netID, 4=date, 5=startTime, 6=endTime).
 * It then returns the desired component of the key
 **/
-function keyDelineator(key,delineator){
+function keyDelimiter(key,delimiter){
 	var keyArray = new Array();
 	keyArray = key.split(" ");
-	if(delineator=="date"){
+	if(delimiter=="date"){
 		return keyArray[4];
 	}
-	else if(delineator=="netID"){
+	else if(delimiter=="netID"){
 		return keyArray[3];
 	}
-	else if(delineator=="lastname"){
+	else if(delimiter=="lastname"){
 		return keyArray[2];
 	}
-	else if(delineator=="firstname"){
+	else if(delimiter=="firstname"){
 		return keyArray[1];
 	}
-	else if(delineator=="dateandtime"){
+	else if(delimiter=="dateandtime"){
 		return keyArray[4]+keyArray[5]+keyArray[6];
 	}
-	else if(delineator=="time"){
+	else if(delimiter=="starttime"){
 		return keyArray[5];
+	}
+	else if(delimiter=="endtime"){
+		return keyArray[6];
 	}
 	return key;
 }
 
-function storeToArray(prepend, searchKey, delineator){
+function storeToArray(prepend, searchKey, delimiter){
 	var index = 0;
 	var array = new Array();
 	for(var i = 0, l = localStorage.length; i<l; i++)
@@ -96,7 +139,7 @@ function storeToArray(prepend, searchKey, delineator){
 		var key = localStorage.key(i);
 		if(stringContains(prepend,key) && stringContains(searchKey,key))
 		{
-			array[index++] = [keyDelineator(key,delineator),localStorage.getItem(key)];
+			array[index++] = [keyDelimiter(key,delimiter),localStorage.getItem(key)];
 		}
 	}
 	return array;
