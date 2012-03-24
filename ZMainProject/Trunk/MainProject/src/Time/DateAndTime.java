28c2553c5d73c180eb50b1d7c02a30d007ef4f8e
Yifei Zhu <zyf@iastate.edu>
2012-03-24 02:01:45 +0000

    add Date.java and modify DateAndTime.java

--- ZMainProject/Trunk/MainProject/src/Time/DateAndTime.java
+++ ZMainProject/Trunk/MainProject/src/Time/DateAndTime.java
@@ -7,35 +7,81 @@ package Time;
  *default--24
  *printing--12 
  */
-public class DateAndTime 
+public class DateAndTime extends Date
 {
 	private int hour;
 	private int minute;
 	private int second;
-	private int month;
-	private int day;
-	private int year;
 	
-	public DateAndTime(int hour, int minute, int second)
-	{
+	
+	public DateAndTime(int year, int month, int day, int hour, int minute, int second) {
+		super(year, month, day);
+//		//time
 		this.hour = hour;
 		this.minute = minute;
 		this.second=second;
 	}
-	
-	public String pritn12Hours()
+
+
+	public int getHour() {
+		return hour;
+	}
+
+
+	public void setHour(int hour) {
+		this.hour = hour;
+	}
+
+
+	public int getMinute() {
+		return minute;
+	}
+
+
+	public void setMinute(int minute) {
+		this.minute = minute;
+	}
+
+
+	public int getSecond() {
+		return second;
+	}
+
+
+	public void setSecond(int second) {
+		this.second = second;
+	}
+
+	public String get24Format()
 	{
 		return hour+":"+minute+":"+second;
 	}
-	
-	
-	public String print24Hours()
+	public String get12Format()
 	{
-		return ;
+		if(this.hour>12)
+		{
+			return (this.hour-12)+":"+this.minute+":"+this.second+"PM";
+		}
+		else
+		{
+			return this.get24Format()+"AM";
+		}
 		
 	}
 	
-	//comparable
+	
+	
+	public String toString(int format) {
+		if(format==12)
+		{
+			return this.toString()+" "+get12Format();
+		}
+		return this.toString()+" "+get24Format();
+	}
+	
+	
+	
+
 	
 	
 
