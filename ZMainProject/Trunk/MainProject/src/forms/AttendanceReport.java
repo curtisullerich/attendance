511d080deeff87393df2a69dab51ba5773b3de9d
Curtis Ullerich <curtisullerich@gmail.com>
2012-03-24 03:13:15 +0000

    added files

---
+++ ZMainProject/Trunk/MainProject/src/forms/AttendanceReport.java
@@ -0,0 +1,31 @@
+package formsPackage;
+
+import java.util.*;
+
+import time.Absence;
+
+
+
+public class AttendanceReport 
+{
+	List<Absence> absences;
+	List<Tardy> tardies;
+	
+	public AttendanceReport()
+	{
+		absences = new LinkedList<Absence>();
+		tardies = new LinkedList<Tardy>();
+	}
+	
+	public void addAbsence(Absence newAbsence)
+	{
+		absences.add(newAbsence);
+	}
+	
+	public void addTardy(Tardy newTardy)
+	{
+		tardies.add(newTardy);
+	}
+	
+
+}
