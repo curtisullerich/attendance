f7ac950cfd6f9fa78b0375f300b69c943fd4c51e
Curtis Ullerich <curtisullerich@gmail.com>
2012-03-24 03:32:28 +0000

    added Forms.java

---
+++ ZMainProject/Trunk/MainProject/src/forms/Form.java
@@ -0,0 +1,14 @@
+package forms;
+
+//Authors: Todd Wegter, Curtis Ullerich, Brandon Maxwell, and Yifei Zhu
+
+import people.Person;
+import time.Date;
+
+public class Form {
+	private Person p;
+	private Date startDate;
+	private Date endDate;
+	private String reason;
+	private boolean Isapproved;
+}
