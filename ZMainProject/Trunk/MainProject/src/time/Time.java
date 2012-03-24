02e145df74d1de3f9a50810558d1a1ccb72a1ffe
Yifei Zhu <zyf@iastate.edu>
2012-03-24 03:45:49 +0000

    modify Time.java

--- ZMainProject/Trunk/MainProject/src/time/Time.java
+++ ZMainProject/Trunk/MainProject/src/time/Time.java
@@ -1,22 +1,28 @@
-package time;
+package TEMPtime;
 /**
  * 
  * @author Yifei Zhu
  *
- *This class contain the time format
+ *This class can be able to 
+ *	-get date, hour, min, second
+ *	-get or print time and date in 12 format and 24 format
+ *	-compare to "date and time"
+ *The default time format is in 24 hours
+ *
  *default--24
  *printing--12 
  */
-public class Time extends Date
+public class Time implements Comparable<Time>
 {
 	private int hour;
 	private int minute;
 	private int second;
+	private Date d;
 	
 	
-	public Time(int year, int month, int day, int hour, int minute, int second) {
-		super(year, month, day);
-		//time
+	public Time(int hour, int minute, int second) {
+	
+//		//time
 		this.hour = hour;
 		this.minute = minute;
 		this.second=second;
@@ -69,6 +75,10 @@ public class Time extends Date
 		
 	}
 	
+	public Date getDate()
+	{
+		return this.d;
+	}
 	
 	
 	public String toString(int format) {
@@ -78,8 +88,40 @@ public class Time extends Date
 		}
 		return this.toString()+" "+get24Format();
 	}
+
 	
+	public int compareTo(Time dt)
+	{
+
+		if(dt==null)
+			return -1 ; //TODO return -1?
 	
+		if(d.compareTo(dt.getDate())!=0)
+		{
+			return d.compareTo(dt.getDate());
+		}
+		//if date are the same
+		
+		//hour is different 
+		if(this.getHour()!=dt.getHour())
+		{
+			return this.getHour()-dt.getHour();
+		}
+		//if also have the 
+		else if(this.getMinute()!=dt.getHour())
+		{
+			return this.getMinute()-dt.getHour();
+		}
+		else if(this.getSecond()!=dt.getSecond())
+		{
+			return this.getSecond()-dt.getSecond();
+		}
+		else 
+		{
+			return 0;
+		}
+	
+	}
 	
 
 	
