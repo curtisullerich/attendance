91309992b3fa45208ae9d7cbcf260f3293b8ecb9
Brandon Maxwell <bmaxwell@iastate.edu>
2012-04-07 04:17:29 +0000

    Servlet for Form A

---
+++ ZMainProject/Trunk/MainProject/src/servlets/FormA.java
@@ -0,0 +1,14 @@
+package servlets;
+
+import java.io.IOException;
+
+import javax.servlet.http.*;
+
+public class FormA extends HttpServlet
+{
+	public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException
+	{
+		
+	}
+
+}
