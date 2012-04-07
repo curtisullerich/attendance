708b3d8f2a9bbbce551207f22e7268cbe06719c4
Curtis Ullerich <curtisullerich@gmail.com>
2012-04-07 22:41:46 +0000

    cpomarator

--- ZMainProject/Trunk/MainProject/src/comparators/FormIsApprovedComp.java
+++ ZMainProject/Trunk/MainProject/src/comparators/FormIsApprovedComp.java
@@ -9,10 +9,11 @@ public class FormIsApprovedComp implements Comparator<Form> {
 	//sorts by unapproved/approved, then by date (larger date first)
 	@Override
 	public int compare(Form o1, Form o2) {
-		if((o1.isApproved() && o2.isApproved()) || (!o1.isApproved() && !o2.isApproved()))
-			return new FormTimeComp().compare(o1, o2);
-		else
-			return o1.isApproved() ? -1 : 1;
+//		if((o1.isApproved() && o2.isApproved()) || (!o1.isApproved() && !o2.isApproved()))
+//			return new FormTimeComp().compare(o1, o2);
+//		else
+//			return o1.isApproved() ? -1 : 1;
+		return 0;//if we need this, then implement it
 	}
 
 }
--- ZMainProject/Trunk/MainProject/war/JSPPages/Director_View_Attendance.jsp
+++ ZMainProject/Trunk/MainProject/war/JSPPages/Director_View_Attendance.jsp
@@ -1,3 +1,7 @@
+<%@ page contentType="text/html; charset=UTF-8" language="java" %>
+<%@ page import="people.*" %>
+<%@ page import="serverLogic.DatabaseUtil" %>
+
 <html>
 	<frameset cols="10%,90%">
 
