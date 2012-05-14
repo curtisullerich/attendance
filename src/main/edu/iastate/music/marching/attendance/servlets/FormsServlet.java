74201481966922177786ceaee6edf12cfd24a376
Daniel Stiner <daniel.stiner@gmail.com>
2012-05-14 06:14:01 +0100

    Basic form layouts, no saving or validation yet

--- src/main/edu/iastate/music/marching/attendance/servlets/FormsServlet.java
+++ src/main/edu/iastate/music/marching/attendance/servlets/FormsServlet.java
@@ -14,10 +14,12 @@ public class FormsServlet extends AbstractBaseServlet {
 		forma, formb, formc, formd, index, view;
 	}
 
+	private static final String SERVLET_PAGE = "form";
+
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
-		if (!isLoggedIn(req, resp, User.Type.Student, User.Type.Director)) {
+		if (!isLoggedIn(req, resp, getServletUserType())) {
 			resp.sendRedirect(AuthServlet.URL_LOGIN);
 			return;
 		}
@@ -30,16 +32,16 @@ public class FormsServlet extends AbstractBaseServlet {
 
 		switch (page) {
 		case forma:
-
+			handleFormA(req, resp);
 			break;
 		case formb:
-
+			handleFormB(req, resp);
 			break;
 		case formc:
-
+			handleFormC(req, resp);
 			break;
 		case formd:
-
+			handleFormD(req, resp);
 			break;
 		case index:
 
@@ -49,10 +51,72 @@ public class FormsServlet extends AbstractBaseServlet {
 			break;
 		default:
 			ErrorServlet.showError(req, resp, 404);
-			return;
-
 		}
 
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		if (!isLoggedIn(req, resp, getServletUserType())) {
+			resp.sendRedirect(AuthServlet.URL_LOGIN);
+			return;
+		}
+
+		Page page = parsePathInfo(req.getPathInfo(), Page.class);
+
+		if (page == null)
+			ErrorServlet.showError(req, resp, 404);
+		else
+			switch (page) {
+			case forma:
+				handleFormA(req, resp);
+				break;
+			case formb:
+				handleFormB(req, resp);
+				break;
+			case formc:
+				handleFormC(req, resp);
+				break;
+			case formd:
+				handleFormD(req, resp);
+				break;
+			default:
+				ErrorServlet.showError(req, resp, 404);
+			}
+	}
+
+	private void handleFormA(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		new PageBuilder(Page.forma, SERVLET_PAGE).passOffToJsp(req, resp);
+	}
+
+	private void handleFormB(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		new PageBuilder(Page.formb, SERVLET_PAGE).passOffToJsp(req, resp);
+	}
+
+	private void handleFormC(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		new PageBuilder(Page.formc, SERVLET_PAGE).passOffToJsp(req, resp);
+	}
+
+	private void handleFormD(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		new PageBuilder(Page.formd, SERVLET_PAGE).passOffToJsp(req, resp);
+	}
+
+	/**
+	 * This servlet can be used for multiple user types, this grabs the type of
+	 * user this specific servlet instance is for
+	 * 
+	 * @return
+	 */
+	private User.Type getServletUserType() {
+		String value = getServletConfig().getInitParameter("userType");
+
+		return User.Type.valueOf(value);
 	}
 
 }
