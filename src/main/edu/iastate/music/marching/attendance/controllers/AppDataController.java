f10e71946884348580da7cd98dcf3cbcf89c4cd8
Daniel Stiner <daniel.stiner@gmail.com>
2012-05-17 20:37:20 +0100

    Changed appdata controller to use twig's returnUnique call

--- src/main/edu/iastate/music/marching/attendance/controllers/AppDataController.java
+++ src/main/edu/iastate/music/marching/attendance/controllers/AppDataController.java
@@ -22,17 +22,17 @@ public class AppDataController extends AbstractController {
 	 */
 	public AppData get() {
 		// TODO Caching
-		QueryResultIterator<AppData> iter = dataTrain.getDataStore().find().type(AppData.class).now();
+		AppData appData = dataTrain.getDataStore().find().type(AppData.class).returnUnique().now();
 		
-		if(!iter.hasNext())
+		if(appData == null)
 		{
 			// No AppData in the data store, so make a new default one
-			AppData appData = ModelFactory.newAppData();
-			return save(appData);
+			appData = ModelFactory.newAppData();
+			appData = save(appData);
 		}
-		else
-			// We have some app data from the store
-			return iter.next();
+		
+		// We have some app data from the store
+		return appData;
 	}
 
 	private AppData save(AppData appData) {
