713b4e68d0923848f82325dd8f3f82872c8432cf
Daniel Stiner <daniel.stiner@gmail.com>
2012-05-18 03:29:54 +0100

    Temporarily forcing mobile password to be password for testing

--- src/main/edu/iastate/music/marching/attendance/App.java
+++ src/main/edu/iastate/music/marching/attendance/App.java
@@ -24,7 +24,11 @@ public class App {
 	}
 
 	public static String getHashedMobilePassword() {
-		return getAppData().getHashedMobilePassword();
+		// HACK @Daniel
+		// For debugging
+		// TODO change back
+		return "5BAA61E4C9B93F3F0682250B6CF8331B7EE68FD8";
+		//return getAppData().getHashedMobilePassword();
 	}
 
 	public static TimeZone getTimeZone() {
