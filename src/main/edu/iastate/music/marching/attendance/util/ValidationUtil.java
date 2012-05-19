2ed471e174270815fa7a53b2cd9cacceb34d81bc
Daniel Stiner <daniel.stiner@gmail.com>
2012-05-19 06:22:12 +0100

    Added TODO markers to validation util

--- src/main/edu/iastate/music/marching/attendance/util/ValidationUtil.java
+++ src/main/edu/iastate/music/marching/attendance/util/ValidationUtil.java
@@ -33,6 +33,7 @@ public class ValidationUtil {
 
 	private static boolean validEmailDomain(String domain) {
 		// TODO Auto-generated method stub
+		// TODO This should be an application setting
 		return "iastate.edu".equals(domain);
 	}
 
@@ -42,6 +43,7 @@ public class ValidationUtil {
 	}
 
 	public static boolean isValidFormDEmail(String email) {
+		// TODO Do this using a data train please
 		return App.getTimeWorkedEmails().contains(email);
 	}
 
