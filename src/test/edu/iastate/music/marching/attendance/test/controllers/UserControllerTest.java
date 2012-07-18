46eda4fda602cdd135dcf6b0001a4a61bb1c5b00
Curtis Ullerich <curtisullerich@gmail.com>
2012-07-18 17:25:18 -0500

    added some tests that don't always pass

--- src/test/edu/iastate/music/marching/attendance/test/controllers/UserControllerTest.java
+++ src/test/edu/iastate/music/marching/attendance/test/controllers/UserControllerTest.java
@@ -3,14 +3,20 @@ package edu.iastate.music.marching.attendance.test.controllers;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 import org.junit.Test;
 
 import com.google.code.twig.ObjectDatastore;
 
+import edu.iastate.music.marching.attendance.controllers.AbsenceController;
 import edu.iastate.music.marching.attendance.controllers.DataTrain;
+import edu.iastate.music.marching.attendance.controllers.EventController;
 import edu.iastate.music.marching.attendance.controllers.UserController;
+import edu.iastate.music.marching.attendance.model.Absence;
+import edu.iastate.music.marching.attendance.model.Event;
 import edu.iastate.music.marching.attendance.model.User;
 import edu.iastate.music.marching.attendance.test.AbstractTest;
 import edu.iastate.music.marching.attendance.test.TestConfig;
@@ -134,4 +140,212 @@ public class UserControllerTest extends AbstractTest {
 		assertEquals(User.Grade.A, uc.averageGrade());
 
 	}
+
+	private static final Date ABSOLUTE_DATE = new Date();
+
+	@Test
+	public void testGrade() {
+		ObjectDatastore datastore = getObjectDataStore();
+
+		DataTrain train = getDataTrain();
+
+		UserController uc = train.getUsersController();
+		EventController ec = train.getEventController();
+		AbsenceController ac = train.getAbsenceController();
+
+		User s1 = createStudent(uc, "student1", 121, "John", "Cox", 2, "major",
+				User.Section.AltoSax);
+
+		// To use separate data store, users need to be associated with it
+		datastore.associate(s1);
+		uc.update(s1);
+
+		// should be A initially
+		assertEquals(User.Grade.A, uc.get(s1.getId()).getGrade());
+
+		Calendar start = Calendar.getInstance();
+		start.setTime(ABSOLUTE_DATE);
+		Calendar end = Calendar.getInstance();
+		end.setTime(ABSOLUTE_DATE);
+		end.roll(Calendar.MINUTE, 50);
+		Event e1 = ec.createOrUpdate(Event.Type.Rehearsal, start.getTime(),
+				end.getTime());
+		Absence a1 = ac.createOrUpdateAbsence(s1, e1);
+		uc.update(s1);
+		assertEquals(User.Grade.B, uc.get(s1.getId()).getGrade());
+
+		start.roll(Calendar.HOUR, 1);
+		end.roll(Calendar.HOUR, 1);
+		Event e2 = ec.createOrUpdate(Event.Type.Rehearsal, start.getTime(),
+				end.getTime());
+		Absence a2 = ac.createOrUpdateAbsence(s1, e2);
+		uc.update(s1);
+		assertEquals(User.Grade.C, uc.get(s1.getId()).getGrade());
+
+		start.roll(Calendar.HOUR, 1);
+		end.roll(Calendar.HOUR, 1);
+		Event e3 = ec.createOrUpdate(Event.Type.Rehearsal, start.getTime(),
+				end.getTime());
+		Absence a3 = ac.createOrUpdateAbsence(s1, e3);
+		uc.update(s1);
+		assertEquals(User.Grade.D, uc.get(s1.getId()).getGrade());
+
+		start.roll(Calendar.HOUR, 1);
+		end.roll(Calendar.HOUR, 1);
+		Event e4 = ec.createOrUpdate(Event.Type.Rehearsal, start.getTime(),
+				end.getTime());
+		Absence a4 = ac.createOrUpdateAbsence(s1, e4);
+		uc.update(s1);
+		assertEquals(User.Grade.F, uc.get(s1.getId()).getGrade());
+
+		start.roll(Calendar.HOUR, 1);
+		end.roll(Calendar.HOUR, 1);
+		Event e5 = ec.createOrUpdate(Event.Type.Rehearsal, start.getTime(),
+				end.getTime());
+		Absence a5 = ac.createOrUpdateAbsence(s1, e5);
+		uc.update(s1);
+		assertEquals(User.Grade.F, uc.get(s1.getId()).getGrade());
+
+		a1.setStatus(Absence.Status.Approved);
+		ac.updateAbsence(a1);
+		assertEquals(User.Grade.F, uc.get(s1.getId()).getGrade());
+		a2.setStatus(Absence.Status.Approved);
+		ac.updateAbsence(a2);
+		assertEquals(User.Grade.D, uc.get(s1.getId()).getGrade());
+		a3.setStatus(Absence.Status.Approved);
+		ac.updateAbsence(a3);
+		assertEquals(User.Grade.C, uc.get(s1.getId()).getGrade());
+		a4.setStatus(Absence.Status.Approved);
+		ac.updateAbsence(a4);
+		assertEquals(User.Grade.B, uc.get(s1.getId()).getGrade());
+		a5.setStatus(Absence.Status.Approved);
+		ac.updateAbsence(a5);
+		assertEquals(User.Grade.A, uc.get(s1.getId()).getGrade());
+		a1.setType(Absence.Type.Tardy);
+		a1.setStatus(Absence.Status.Denied);
+		ac.updateAbsence(a1);
+		assertEquals(User.Grade.Aminus, uc.get(s1.getId()).getGrade());
+		a2.setStatus(Absence.Status.Denied);
+		ac.updateAbsence(a2);
+		assertEquals(User.Grade.Bminus, uc.get(s1.getId()).getGrade());
+		
+		start.roll(Calendar.HOUR, 1);
+		end.roll(Calendar.HOUR, 1);
+		Calendar middle = (Calendar) start.clone();
+		middle.roll(Calendar.MINUTE, 10);
+
+		//creating an un-anchored tardy
+		Absence a6 = ac.createOrUpdateTardy(s1, middle.getTime());
+		ac.updateAbsence(a6);
+		assertEquals(User.Grade.Bminus, uc.get(s1.getId()).getGrade());
+		
+		middle.roll(Calendar.MINUTE,10);
+		Absence a7 = ac.createOrUpdateEarlyCheckout(s1, middle.getTime());
+		ac.updateAbsence(a7);
+		assertEquals(User.Grade.Bminus, uc.get(s1.getId()).getGrade());
+		
+		Event e6 = ec.createOrUpdate(Event.Type.Rehearsal, start.getTime(), end.getTime());
+		a6.setEvent(e6);
+		ac.updateAbsence(a6);
+		assertEquals(User.Grade.Cplus, uc.get(s1.getId()).getGrade());
+
+		a7.setEvent(e6);
+		ac.updateAbsence(a7);
+		assertEquals(User.Grade.C, uc.get(s1.getId()).getGrade());
+		
+	}
+	
+	
+	@Test
+	public void nonOverLappingAbsencesTest() {
+		ObjectDatastore datastore = getObjectDataStore();
+
+		DataTrain train = getDataTrain();
+
+		UserController uc = train.getUsersController();
+		EventController ec = train.getEventController();
+		AbsenceController ac = train.getAbsenceController();
+
+		User s1 = createStudent(uc, "student1", 121, "John", "Cox", 2, "major",
+				User.Section.AltoSax);
+
+		// To use separate data store, users need to be associated with it
+		datastore.associate(s1);
+		uc.update(s1);
+
+		Calendar start = Calendar.getInstance();
+		start.setTime(ABSOLUTE_DATE);
+		Calendar end = Calendar.getInstance();
+		end.setTime(ABSOLUTE_DATE);
+		end.roll(Calendar.MINUTE, 50);
+
+		Calendar tardy = (Calendar) start.clone();
+		Calendar early = (Calendar) start.clone();
+
+		tardy.roll(Calendar.MINUTE, 10);
+		early.roll(Calendar.MINUTE, 30);
+
+		//creating an un-anchored tardy
+		Absence a6 = ac.createOrUpdateTardy(s1, tardy.getTime());
+		ac.updateAbsence(a6);
+		
+		Absence a7 = ac.createOrUpdateEarlyCheckout(s1, early.getTime());
+		ac.updateAbsence(a7);
+		
+		Event e6 = ec.createOrUpdate(Event.Type.Rehearsal, start.getTime(), end.getTime());
+		a6.setEvent(e6);
+		a6 = ac.updateAbsence(a6);
+		assertEquals(a6.getType(), Absence.Type.Tardy);
+
+		a7.setEvent(e6);
+		a7 = ac.updateAbsence(a7);
+		assertEquals(a7.getType(), Absence.Type.EarlyCheckOut);
+	}
+	
+	@Test
+	public void overLappingAbsencesTest() {
+		ObjectDatastore datastore = getObjectDataStore();
+
+		DataTrain train = getDataTrain();
+
+		UserController uc = train.getUsersController();
+		EventController ec = train.getEventController();
+		AbsenceController ac = train.getAbsenceController();
+
+		User s1 = createStudent(uc, "student1", 121, "John", "Cox", 2, "major",
+				User.Section.AltoSax);
+
+		// To use separate data store, users need to be associated with it
+		datastore.associate(s1);
+		uc.update(s1);
+
+		Calendar start = Calendar.getInstance();
+		start.setTime(ABSOLUTE_DATE);
+		Calendar end = Calendar.getInstance();
+		end.setTime(ABSOLUTE_DATE);
+		end.roll(Calendar.MINUTE, 50);
+
+		Calendar tardy = (Calendar) start.clone();
+		Calendar early = (Calendar) start.clone();
+
+		tardy.roll(Calendar.MINUTE, 30);
+		early.roll(Calendar.MINUTE, 10);
+
+		//creating an un-anchored tardy
+		Absence a6 = ac.createOrUpdateTardy(s1, tardy.getTime());
+		ac.updateAbsence(a6);
+		
+		Absence a7 = ac.createOrUpdateEarlyCheckout(s1, early.getTime());
+		ac.updateAbsence(a7);
+		
+		Event e6 = ec.createOrUpdate(Event.Type.Rehearsal, start.getTime(), end.getTime());
+		a6.setEvent(e6);
+		a6 = ac.updateAbsence(a6);
+		assertEquals(a6.getType(), Absence.Type.Tardy);
+
+		a7.setEvent(e6);
+		a7 = ac.updateAbsence(a7);
+		assertEquals(a7.getType(), Absence.Type.Absence);
+	}
+
 }
