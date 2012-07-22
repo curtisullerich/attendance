05a869a98dc8e5ca1cdba1fd3c19673373ba3494
Daniel Stiner <danstiner@gmail.com>
2012-07-22 08:05:20 -0500

    Form B auto-approval testing #66

--- src/test/edu/iastate/music/marching/attendance/test/integration/FormAutoApproval.java
+++ src/test/edu/iastate/music/marching/attendance/test/integration/FormAutoApproval.java
@@ -16,12 +16,12 @@ import edu.iastate.music.marching.attendance.model.User;
 import edu.iastate.music.marching.attendance.test.AbstractTest;
 import edu.iastate.music.marching.attendance.test.util.Users;
 
-public class FormSubmission extends AbstractTest {
+public class FormAutoApproval extends AbstractTest {
 
 	private static final Date ABSOLUTE_DATE = new Date();
 
 	@Test
-	public void AutoApproveAbsence_Succeeds_FromFormA() {
+	public void AutoApproveAbsence_Succeeds_FormA_FormSecond() {
 		createUsersHelper();
 		DataTrain train = DataTrain.getAndStartTrain();
 
@@ -51,7 +51,34 @@ public class FormSubmission extends AbstractTest {
 	}
 
 	@Test
-	public void AutoApproveAbsence_Succeeds_FromFormB() {
+	public void AutoApproveAbsence_Succeeds_FormA_FormFirst() {
+		createUsersHelper();
+		DataTrain train = DataTrain.getAndStartTrain();
+
+		Calendar c = Calendar.getInstance();
+		c.setTime(ABSOLUTE_DATE);
+		c.roll(Calendar.HOUR, 1);
+
+		// Create and approve form to excuse absence
+		Form form = train.getFormsController().createFormA(student1,
+				ABSOLUTE_DATE, "reason");
+		train.getFormsController().approve(form);
+
+		// Create a 1 hour event for absence
+		train.getEventController().createOrUpdate(Event.Type.Performance,
+				ABSOLUTE_DATE, c.getTime());
+
+		// Create a 1 hour absence
+		Absence a = train.getAbsenceController().createOrUpdateAbsence(
+				student1, ABSOLUTE_DATE, c.getTime());
+
+		// Verify both form and absence are now approved
+		assertEquals(Form.Status.Approved, form.getStatus());
+		assertEquals(Absence.Status.Approved, a.getStatus());
+	}
+
+	@Test
+	public void AutoApproveAbsence_Succeeds_FormB_ExactOverlap() {
 		createUsersHelper();
 		DataTrain train = DataTrain.getAndStartTrain();
 
@@ -71,8 +98,10 @@ public class FormSubmission extends AbstractTest {
 		assertEquals(Absence.Status.Pending, a.getStatus());
 
 		// Create and approve form to excuse absence
-		Form form = train.getFormsController().createFormA(student1,
-				ABSOLUTE_DATE, "reason");
+		int day = c.get(Calendar.DAY_OF_WEEK);
+		Form form = train.getFormsController().createFormB(student1,
+				"department", "course", "section", "building", ABSOLUTE_DATE,
+				ABSOLUTE_DATE, day, ABSOLUTE_DATE, c.getTime(), "details", 0);
 		train.getFormsController().approve(form);
 
 		// Verify both form and absence are now approved
@@ -245,7 +274,7 @@ public class FormSubmission extends AbstractTest {
 		// Create a 1 hour absence
 		Absence a = train.getAbsenceController().createOrUpdateAbsence(
 				student1, ABSOLUTE_DATE, c.getTime());
-		
+
 		a.setStatus(Absence.Status.Denied);
 
 		// Create and approve form to excuse absence
@@ -269,14 +298,14 @@ public class FormSubmission extends AbstractTest {
 	private void createUsersHelper() {
 		UserController uc = DataTrain.getAndStartTrain().getUsersController();
 
-		student1 = Users.createStudent(uc, "student1", 55055050, "First", "Last", 1,
-				"major", User.Section.AltoSax);
+		student1 = Users.createStudent(uc, "student1", 55055050, "First",
+				"Last", 1, "major", User.Section.AltoSax);
 
-		student2 = Users.createStudent(uc, "student2", 55055052, "First", "Last", 2,
-				"major", User.Section.AltoSax);
+		student2 = Users.createStudent(uc, "student2", 55055052, "First",
+				"Last", 2, "major", User.Section.AltoSax);
 
-		student3 = Users.createStudent(uc, "student3", 55055053, "First", "Last", 3,
-				"major", User.Section.AltoSax);
+		student3 = Users.createStudent(uc, "student3", 55055053, "First",
+				"Last", 3, "major", User.Section.AltoSax);
 
 		ta = Users.createTA(uc, "ta", 55050050, "Fisrt", "Last", 4, "major",
 				User.Section.AltoSax);
