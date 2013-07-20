5a57ee148edeed2dc6e52f38e69bad0d00710e3b
Daniel Stiner <danstiner@gmail.com>
2013-07-19 23:34:24 -0700

    Magic caching

--- src/main/edu/iastate/music/marching/attendance/model/store/AttendanceDatastore.java
+++ src/main/edu/iastate/music/marching/attendance/model/store/AttendanceDatastore.java
@@ -1,17 +1,17 @@
 package edu.iastate.music.marching.attendance.model.store;
 
+import com.google.code.twig.LoadCommand.CacheMode;
 import com.google.code.twig.Settings;
 import com.google.code.twig.annotation.AnnotationConfiguration;
 import com.google.code.twig.standard.StandardObjectDatastore;
-
 
 public class AttendanceDatastore extends StandardObjectDatastore {
 
 	public static final int VERSION = 1;
 	public static final String MODEL_OBJECTS_PACKAGE = "edu.iastate.music.marching.attendance.model.store";
-	
+
 	static final AnnotationConfiguration CONFIGURATION;
-	
+
 	static {
 		AnnotationConfiguration a = new AnnotationConfiguration();
 		a.register(Absence.class);
@@ -22,9 +22,14 @@ public class AttendanceDatastore extends StandardObjectDatastore {
 		a.register(MobileDataUpload.class);
 		a.register(User.class);
 		CONFIGURATION = a;
+
+		AttendanceDatastore.registerCachedKind(a.typeToKind(User.class), 6000,
+				1000, true, true);
+		AttendanceDatastore.registerCachedKind(a.typeToKind(Event.class), 6000,
+				1000, true, true);
 	}
-	
+
 	public AttendanceDatastore() {
-		super(Settings.builder().build(), CONFIGURATION, 1, true);
+		super(Settings.builder().build(), CONFIGURATION, 0, true);
 	}
 }
